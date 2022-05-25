6bb0c60da11e78583f79bcc216f48904fa2176c5
wentam <wentam42@gmail.com>
2022-05-25 09:14:59 -0400

    swmo module init

---
+++ modules/swmo/default.nix
@@ -0,0 +1,62 @@
+{config, options, lib, pkgs, ...}:
+
+let
+  sxmopkgs = import ../../default.nix { inherit pkgs; };
+in
+{
+  imports = [];
+  options = {
+    services.xserver.desktopManager.swmo = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "";
+      };
+    };
+ };
+
+ config = lib.mkIf config.services.xserver.desktopManager.swmo.enable {
+   environment.systemPackages = with pkgs; [
+     sxmopkgs.sxmo-utils
+     sxmopkgs.sxmo-dwm
+     sxmopkgs.sxmo-st
+     sxmopkgs.sxmo-dmenu
+     sxmopkgs.superd
+     sway
+     busybox
+     bemenu
+     lisgd
+     svkbd
+     inotify-tools
+     pn
+     wvkbd
+     gojq
+   ];
+
+   fonts.fonts = [ pkgs.nerdfonts ];
+
+   programs.sway = {
+     enable = true;
+     wrapperFeatures.gtk = true; # so that gtk works properly
+     extraPackages = with pkgs; [];
+   };
+
+   # Set default sway config to the sxmo template
+   environment.etc."sway/config".source = "${sxmopkgs.sxmo-utils}/share/sxmo/appcfg/sway_template";
+
+   # $TERMCMD used by the sxmo sway template config
+   environment.variables.TERMCMD = "st";
+
+   # Power button shouldn't immediately power off the device
+   # TODO: This change could apply to other sessions. It'd better find a way to do this at session start.
+   services.logind.extraConfig = ''
+       HandlePowerKey=ignore
+   '';
+
+   # Install udev rules
+   services.udev.packages = [ sxmopkgs.sxmo-utils ];
+
+   # Define session
+   services.xserver.displayManager.sessionPackages = [ sxmopkgs.sxmo-utils ];
+ };
+}
